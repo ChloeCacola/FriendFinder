14de647fa39f1bebeb16e3797f7fc97936d87738
ChloeCacola <cscorrigan@icloud.com>
2017-03-09 16:13:44 -0500

    finding friend calculation working WITH raw modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,13 +10,44 @@ module.exports = function(app) {
 	});
 
 	//handle incoming survey results & compatibility logic (post)
-	app.post("/api/friends", function(req, res) {
-		var newfriendTest = req.body
-		console.log(newfriendTest);
+	app.post("/api/friends", function(req,res) {
+		var rawFriend = req.body;
+		var rawScore = rawFriend.scores
+		var numScore = [];
+		var matches = [];
+		for(i=0; i<rawScore.length;i++) {
+			numScore.push(parseInt(rawScore[i]));
+		}
 
-		//compare compatibility here:
-		//Math.abs(newfriend minus result)
-		//loop through the new friend to compare numbers
+		console.log(numScore);
+
+		totalDiff = 0;
+		for(i=0; i<friendData.length; i++) {
+			console.log(friendData[i].scores);
+			console.log(numScore);
+				for(n=0; n<friendData[i].scores.length; n++) {
+						diff = Math.abs(friendData[i].scores[n] - numScore[n]);
+					
+					console.log(diff);
+					totalDiff += diff
+
+				}
+			matches.push({
+				name: friendData[i].name,
+				photo: friendData[i].photo,
+				totaldiff: totalDiff});
+			console.log(totalDiff);
+			totalDiff = 0
+		}
+		console.log(matches);
+		matches.sort(function(a,b) {
+			return a.totaldiff - b.totaldiff;
+		});
+		console.log("-----------------")
+		console.log(matches[0]);
+		match = JSON.stringify(matches[0]);
+		res.json(match);
+
 	});
 }
 
